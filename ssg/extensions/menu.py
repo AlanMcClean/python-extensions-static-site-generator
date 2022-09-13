eee1ba316b134ece4d3c91df21aac94f4717ab81
AlanMcClean <87909322+AlanMcClean@users.noreply.github.com>
2022-09-13 16:19:21 +0100

    menu ext pass 2

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -1,10 +1,21 @@
 from ssg import hooks, parsers
 
+files = []
 
+@hooks.register("collect_files")
 def collect_files(source, site_parsers):
-    hooks.register(collect_files)
     valid = lambda p: not isinstance(p, parsers.ResourceParser)
     for path in source.rglob("*"):
-        for parser in list(filter(valid,site_parsers))):
+        for parser in list(filter(valid,site_parsers)):
             if parser.valid_file_ext(path.suffix):
                 files.append(path)
+
+
+@hooks.register("generate_menu")
+def generate_menu(html, ext):
+    template = '<li><a href="{}{}">{}</a></li>'
+    menu_item = lambda name,ext: template.format(name, ext, name.titlecase())
+
+    menu = list(menu_item(path.stem, ext) for path in files).join("\n")
+
+    return "<ul>\n{}<ul>\n{}".format(menu, html)
